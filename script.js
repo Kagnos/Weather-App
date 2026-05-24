57f80b983202cca67a40ef095969b8dee8edfeb6
Kagnos <kagcodes@gmail.com>
2026-05-24 11:11:43 -0500

    Add search functionality

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/dayton?key=FRKPALS6NRC22D5HDJUWEA5ZA")
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(response) {
-      console.log(response.description)
-    });
+const searchBtn = document.querySelector("#search-btn");
+const searchInput = document.querySelector("#search-input");
+
+async function fetchWeather(search) {
+    try {
+        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${search}?key=FRKPALS6NRC22D5HDJUWEA5ZA`);
+        const data = await response.json();
+        console.log(data.address)
+    } catch (error) {
+        console.error(error);
+    };
+};
+
+searchBtn.addEventListener("click", () => {
+    fetchWeather(searchInput.value);
+    searchInput.value = "";
+});
