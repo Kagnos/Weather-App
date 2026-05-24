b6da69be23e2a7f9b5c9a984673f7ba9ae3fabdf
Kagnos <kagcodes@gmail.com>
2026-05-24 10:39:37 -0500

    Create api fetch function

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log("Page loaded and script running!");
-});
+fetch("https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/dayton?key=FRKPALS6NRC22D5HDJUWEA5ZA")
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(response) {
+      console.log(response.description)
+    });
