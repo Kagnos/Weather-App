48b777abbf1e5f4bf9bdfabf8380dfe319b120d6
Kagnos <kagcodes@gmail.com>
2026-05-26 10:10:36 -0500

    Create renderWeather function and style page

--- script.js
+++ script.js
@@ -5,10 +5,38 @@ async function fetchWeather(search) {
     try {
         const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${search}?key=FRKPALS6NRC22D5HDJUWEA5ZA`);
         const data = await response.json();
-        console.log(data.address)
+
+        renderWeather(data);
     } catch (error) {
         console.error(error);
     };
+};
+
+function renderWeather(data) {
+    const weatherContainer = document.querySelector("#weather-container");
+    const h1 = document.createElement("h1");
+    const h2 = document.createElement("h2");
+    const p = document.createElement("p");
+
+    const address = h1.cloneNode();
+    address.innerText = data.resolvedAddress;
+    weatherContainer.append(address);
+
+    const temp = h2.cloneNode();
+    temp.innerText = `Temp: ${data.currentConditions.temp}`;
+    weatherContainer.append(temp);
+
+    const feelsLike = h2.cloneNode();
+    feelsLike.innerText = `Feels Like: ${data.currentConditions.feelslike}`;
+    weatherContainer.append(feelsLike);
+
+    const humidity = h2.cloneNode();
+    humidity.innerText = `Humidity: ${data.currentConditions.humidity}`;
+    weatherContainer.append(humidity);
+
+    const description = p.cloneNode();
+    description.innerText = data.description;
+    weatherContainer.append(description);
 };
 
 searchBtn.addEventListener("click", () => {
