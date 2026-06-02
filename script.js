f346b771cc0522059cb18508a3086b40236fed62
Kagnos <kagcodes@gmail.com>
2026-06-02 13:30:23 -0500

    Create and style hourly weather

--- script.js
+++ script.js
@@ -42,14 +42,111 @@ function renderWeather(data) {
             <h2>Temp: ${data.currentConditions.temp}${tempUnit}</h2>
             <h2>Feels Like: ${data.currentConditions.feelslike}${tempUnit}</h2>
             <h2>Humidity: ${data.currentConditions.humidity}%</h2>
-            <p>${data.description}</p>
+            <h3>${data.description}</h3>
         </div>
 
         <div id="hours">
-        <h1>${data.days[0].hours[0].temp}</h1>
+            <h1>Hourly Weather</h1>
+            <div>
+                <h2>12 AM</h2>
+                <h3>${data.days[0].hours[0].temp}${tempUnit}</h3>
+            </div>
+            <div>
+                <h2>1 AM</h2>
+                <h3>${data.days[0].hours[1].temp}${tempUnit}</h3>
+            </div>
+            <div>
+                <h2>2 AM</h2>
+                <h3>${data.days[0].hours[2].temp}${tempUnit}</h3>
+            </div>
+            <div>
+                <h2>3 AM</h2>
+                <h3>${data.days[0].hours[3].temp}${tempUnit}</h3>
+            </div>
+            <div>
+                <h2>4 AM</h2>
+                <h3>${data.days[0].hours[4].temp}${tempUnit}</h3>
+            </div>
+            <div>
+                <h2>5 AM</h2>
+                <h3>${data.days[0].hours[5].temp}${tempUnit}</h3>
+            </div>
+            <div>
+                <h2>6 AM</h2>
+                <h3>${data.days[0].hours[6].temp}${tempUnit}</h3>
+            </div>
+            <div>
+                <h2>7 AM</h2>
+                <h3>${data.days[0].hours[7].temp}${tempUnit}</h3>
+            </div>
+            <div>
+                <h2>8 AM</h2>
+                <h3>${data.days[0].hours[8].temp}${tempUnit}</h3>
+            </div>
+            <div>
+                <h2>9 AM</h2>
+                <h3>${data.days[0].hours[9].temp}${tempUnit}</h3>
+            </div>
+            <div>
+                <h2>10 AM</h2>
+                <h3>${data.days[0].hours[10].temp}${tempUnit}</h3>
+            </div>
+            <div>
+                <h2>11 AM</h2>
+                <h3>${data.days[0].hours[11].temp}${tempUnit}</h3>
+            </div>
+            <div>
+                <h2>12 PM</h2>
+                <h3>${data.days[0].hours[12].temp}${tempUnit}</h3>
+            </div>
+            <div>
+                <h2>1 PM</h2>
+                <h3>${data.days[0].hours[13].temp}${tempUnit}</h3>
+            </div>
+            <div>
+                <h2>2 PM</h2>
+                <h3>${data.days[0].hours[14].temp}${tempUnit}</h3>
+            </div>
+            <div>
+                <h2>3 PM</h2>
+                <h3>${data.days[0].hours[15].temp}${tempUnit}</h3>
+            </div>
+            <div>
+                <h2>4 PM</h2>
+                <h3>${data.days[0].hours[16].temp}${tempUnit}</h3>
+            </div>
+            <div>
+                <h2>5 PM</h2>
+                <h3>${data.days[0].hours[17].temp}${tempUnit}</h3>
+            </div>
+            <div>
+                <h2>6 PM</h2>
+                <h3>${data.days[0].hours[18].temp}${tempUnit}</h3>
+            </div>
+            <div>
+                <h2>7 PM</h2>
+                <h3>${data.days[0].hours[19].temp}${tempUnit}</h3>
+            </div>
+            <div>
+                <h2>8 PM</h2>
+                <h3>${data.days[0].hours[20].temp}${tempUnit}</h3>
+            </div>
+            <div>
+                <h2>9 PM</h2>
+                <h3>${data.days[0].hours[21].temp}${tempUnit}</h3>
+            </div>
+            <div>
+                <h2>10 PM</h2>
+                <h3>${data.days[0].hours[22].temp}${tempUnit}</h3>
+            </div>
+            <div>
+                <h2>11 PM</h2>
+                <h3>${data.days[0].hours[23].temp}${tempUnit}</h3>
+            </div>
         </div>
 
         <div id="week">
+        <h1>This Week</h1>
         <h1>${data.days[1].temp}</h1>
         </div>
     `;
